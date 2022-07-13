34dafcad04c98dda0231d0afc13906524da612cb
Pablo Andreu <99558817+pandma@users.noreply.github.com>
2022-07-13 22:58:57 +0200

    Conatact_user model udated

--- fiva/app/models/Contact_User.py
+++ fiva/app/models/Contact_User.py
@@ -8,3 +8,5 @@ class Constact_User(models.Model):
     product = models.CharField(max_length=100)
     message = models.CharField(max_length=255)
     state = models.CharField(max_length=100, default='Pocessing')
+
+    
