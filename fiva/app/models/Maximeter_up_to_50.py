497b77cfe3dc5146998c0daa829c42b0354db028
Pablo Andreu <99558817+pandma@users.noreply.github.com>
2022-07-15 21:07:57 +0200

    max50 model updated

--- fiva/app/models/Maximeter_up_to_50.py
+++ fiva/app/models/Maximeter_up_to_50.py
@@ -9,6 +9,7 @@ class Maximeter_up_to_50(models.Model):
     nif = models.CharField(max_length=100)
     quarter_hour_curve = models.CharField(max_length=9999)
     hired_power = models.CharField(max_length=100)
+    hired_price = models.CharField(max_length=100)
     anual_consumption = models.CharField(max_length=100, blank=True)
     optimal_anual_consumption = models.CharField(max_length=100, blank=True)
     annual_savings = models.CharField(max_length=100, blank=True)
